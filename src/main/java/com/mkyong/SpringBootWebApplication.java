ac6f9feb6552944039146741fd40e2f398027ae3
SIVA <simha@gmail.com>
2018-08-15 10:58:16 +0530

    adding

---
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -0,0 +1,21 @@
+package com.mkyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+//http://www.thymeleaf.org/doc/articles/layouts.html
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootWebApplication.class);
+    }
+
+}
